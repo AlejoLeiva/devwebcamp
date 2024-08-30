36086eefea010477942cac39ece18953fc34ea5b
AlejoLeiva <joher.leiva@gmail.com>
2024-08-29 19:23:49 -0500

    Panel de admninitracion

---
+++ views/templates/header.php
@@ -0,0 +1,34 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Login</a>
+        </nav>
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">&#60; DevWebCamp /></h1>
+            </a>
+
+            <p class="header__texto">Marzo 7-8 de 2024</p>
+            <p class="header__texto header__texto--modalidad">En Línea - Presencial</p>
+
+            <a href="/registro" class="header__boton">Comprar Boleta</a>
+
+        </div>
+    </div>
+</header>
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">
+                &#60; DevWebCamp />
+            </h2>
+        </a>
+        <nav class="navegacion">
+            <a class="navegacion__enlace" href="/devwebcamp">Evento</a>
+            <a class="navegacion__enlace" href="/paquetes">Paquetes</a>
+            <a class="navegacion__enlace" href="/conferencias">Workshop / Conferencias</a>
+            <a class="navegacion__enlace" href="/registro">Comprar Boleta</a>
+        </nav>
+    </div>
+</div>
